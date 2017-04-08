2d51ab842ddc43b84bf9182557401bcc8707e3a9
koluong <kennyk94@live.com>
2017-04-08 14:23:55 -0700

    Update boilerplate with title

--- itc-mbs/src/app/app.component.ts
+++ itc-mbs/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'MISSA ITC 2017 | MB Solutions';
 }
