cf13633021637cbaeacee282fbb022da9fe7349d
Kenny Korn <kennykorn@gmail.com>
2017-04-16 01:10:07 -0700

    Basic Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MapComponent } from './map/map.component';
+import { WeatherExpandComponent } from './weather/weather-expand/weather-expand.component';
 
 const appRoutes: Routes = [
-  {path: '', component: MapComponent }
+  {path: '', component: MapComponent, children: [
+    {path: 'weather', component: WeatherExpandComponent }
+  ]}
 ];
 
 @NgModule({
